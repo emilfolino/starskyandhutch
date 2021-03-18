e18166a0279a7c1b6966524c2673409fb8d71b39
Emil Folino <emilfolino@gmail.com>
2021-03-18 13:49:15 +0100

    fixed rest

--- student.js
+++ student.js
@@ -1,23 +1,31 @@
-const { readdir, mkdir, readFile, writeFile, copyFile } = require('fs');
+const fsPromises = require('fs').promises;
 
 const inPath = "./infiles";
 const outPath = "./outfiles";
 
-const header = `<!DOCTYPE html>
+const headerIndex = `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Starsky And Hutch</title>
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/atom-one-light.min.css">
     <link rel="stylesheet" href="../style.css">
 </head>
-<body>
-<main class="content">
-    <article>`;
+<body>`;
 
-const footer = `</article>
-</main>
-<footer class="footer">
+const headerCompare = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Starsky And Hutch</title>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/themes/prism-okaidia.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/plugins/line-numbers/prism-line-numbers.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/plugins/line-highlight/prism-line-highlight.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>`;
+
+const footerIndex = `<footer class="footer">
     <div class="inner-footer">
         <p>&copy; <a href="https://dbwebb.se">dbwebb</a></p>
     </div>
@@ -25,158 +33,174 @@ const footer = `</article>
 </body>
 </html>`;
 
-function parseFiles() {
+const footerCompare = `<footer class="footer">
+    <div class="inner-footer">
+        <p>&copy; <a href="https://dbwebb.se">dbwebb</a></p>
+    </div>
+</footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/components/prism-core.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/plugins/autoloader/prism-autoloader.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/plugins/line-highlight/prism-line-highlight.min.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/plugins/line-numbers/prism-line-numbers.min.js" type="text/javascript"></script>
+</body>
+</html>`;
+
+async function parseFiles() {
     const now = new Date().toJSON().substring(0, 19);
+    const files = await fsPromises.readdir(inPath);
 
-    readdir(inPath, function (err, files) {
-        if (err) {
-            console.error(err);
-        }
-
-        files.forEach((course, i) => {
-            parseCourse(course, now);
-        });
+    files.forEach((course, i) => {
+        parseCourse(course, now);
     });
 }
 
-function parseCourse(course, now) {
+async function parseCourse(course, now) {
     const resultDir = inPath + "/" + course + "/result";
     const submissionsDir = inPath + "/" + course + "/submissions";
-
     const outCourseDir = outPath + "/" + course + "_" + now;
 
-    mkdir(outCourseDir, function(err) {
-        if (err) {
-            console.error(err);
-        }
+    await fsPromises.mkdir(outCourseDir);
 
-        readdir(resultDir, function (err, kmoms) {
-            if (err) {
-                console.error(err);
-            }
+    const kmoms = await fsPromises.readdir(resultDir);
 
-            kmoms.forEach((kmom, i) => {
-                parseKMOM(kmom, resultDir, submissionsDir, outCourseDir);
-            });
-        });
+    kmoms.forEach((kmom, i) => {
+        parseKMOM(kmom, resultDir, submissionsDir, outCourseDir);
     });
 }
 
-function parseKMOM(
+async function parseKMOM(
     kmom,
     resultDir,
     submissionsDir,
     outCourseDir
 ) {
     const resultFile = resultDir + "/" + kmom + "/computer_matches.csv";
+    const data = await fsPromises.readFile(resultFile, 'utf8');
+    const lines = data.split("\n").filter(line => line);
 
-    readFile(resultFile, 'utf8' , (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-        const lines = data.split("\n").filter(line => line);
-
-        createIndex(outCourseDir, kmom, lines, submissionsDir);
-    });
+    createIndex(outCourseDir, kmom, lines, submissionsDir);
 }
 
-function createIndex(outCourseDir, kmom, students, submissionsDir) {
+async function createIndex(outCourseDir, kmom, students, submissionsDir) {
     const outDir = outCourseDir + "/" + kmom;
 
-    mkdir(outDir, function(err) {
-        if (err) {
-            console.error(err);
-            return;
-        }
+    await fsPromises.mkdir(outDir);
+    await fsPromises.copyFile("./public/style.css", outCourseDir + "/style.css");
 
-        copyFile("./public/style.css", outCourseDir + "/style.css", (err) => {
-            if (err) {
-                throw err;
-            }
-        });
+    let htmlContent = headerIndex;
 
-        let htmlContent = header;
+    htmlContent += `<main class="content">
+        <article>`;
+    htmlContent += `<h1>Studenter: ${outDir}</h1>`;
+    htmlContent += `<table>
+                        <thead>
+                            <tr>
+                                <th>Student 1</th>
+                                <th>Student 2</th>
+                                <th>Procent likhet</th>
+                                <th>Procent likhet</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>`;
 
-        htmlContent += `<h1>Studenter: ${outDir}</h1>`;
-        htmlContent += `<table>
-                            <thead>
-                                <tr>
-                                    <th>Student 1</th>
-                                    <th>Student 2</th>
-                                    <th>Procent likhet</th>
-                                    <th>Procent likhet</th>
-                                </tr>
-                            </thead>
-                            <tbody>`;
+    students.forEach((student, i) => {
+        createComparison(outCourseDir, kmom, student, submissionsDir);
 
-        students.forEach((student, i) => {
-            createComparison(outCourseDir, kmom, student, submissionsDir);
+        const data = student.split(";");
 
-            const data = student.split(";");
-
-            htmlContent += `<tr>
-                                <td>${data[0]}</td>
-                                <td>${data[1]}</td>
-                                <td>${data[2]}</td>
-                                <td>${data[3]}</td>
-                            </tr>`;
-        });
-
-        htmlContent += `    </tbody>
-                        </table>`;
-
-        htmlContent += footer;
-
-        writeFile(outDir + "/index.html", htmlContent, err => {
-            if (err) {
-                console.error(err)
-                return
-            }
-
-
-        });
+        htmlContent += `<tr>
+                            <td>${data[0]}</td>
+                            <td>${data[1]}</td>
+                            <td>${data[2]}</td>
+                            <td>${data[3]}</td>
+                            <td><a href="${data[0]}_${data[1]}.html">See comparison</a></td>
+                        </tr>`;
     });
+
+    htmlContent += `    </tbody>
+                    </table>`;
+    htmlContent += `</article>
+                </main>`;
+    htmlContent += footerIndex;
+
+    await fsPromises.writeFile(outDir + "/index.html", htmlContent);
+    console.log("Created: " + outDir + "/index.html");
 }
 
-function createComparison(outCourseDir, kmom, student, submissionsDir) {
+async function createComparison(
+    outCourseDir,
+    kmom,
+    student,
+    submissionsDir
+) {
     const filesDir = submissionsDir + "/" + kmom;
+    const fields = student.split(";").filter(field => field);
+    const fileName = fields[0] + "_" + fields[1] + ".html";
+    const entireFileName = outCourseDir + "/" + kmom + "/" + fileName;
 
-    const fields = student.split(";");
+    let htmlContent = headerCompare;
 
-    let htmlContent = header;
-
+    htmlContent += `<div class='outer-container'>`;
     htmlContent += `<h1>${fields[0]} vs. ${fields[1]}</h1>`;
-    htmlContent += `<p>${fields[2]} vs. ${fields[3]}</p>`;
+    htmlContent += `<p>Likhet: ${fields[2]} vs. ${fields[3]}</p>`;
 
     const codeRelatedFields = fields.slice(4);
+    let leftFilesToLoad = [];
+    let rightFilesToLoad = [];
+    let studentFileName = "";
 
     for (let i = 0; i < codeRelatedFields.length; i += 6) {
+        studentFileName = `${filesDir}/${fields[0]}/${codeRelatedFields[i]}`;
 
+        if (!leftFilesToLoad.hasOwnProperty(studentFileName)) {
+            leftFilesToLoad[studentFileName] = [];
+        }
+
+        leftFilesToLoad[studentFileName].push(`${codeRelatedFields[i + 1]}-${codeRelatedFields[i + 2]}`);
+
+
+
+        studentFileName = `${filesDir}/${fields[1]}/${codeRelatedFields[i + 3]}`;
+
+        if (!rightFilesToLoad.hasOwnProperty(studentFileName)) {
+            rightFilesToLoad[studentFileName] = [];
+        }
+
+        rightFilesToLoad[studentFileName].push(`${codeRelatedFields[i + 4]}-${codeRelatedFields[i + 5]}`);
     }
 
-    // htmlContent += "<div class='code-container'>";
-    //     htmlContent += "<div class='left-code'>";
-    //     htmlContent += leftCode;
-    //     htmlContent += "</div>";
-    //     htmlContent += "<div class='right-code'>";
-    //     htmlContent += rightCode;
-    //     htmlContent += "</div>";
-    // htmlContent += "</div>";
+    const leftCode = await getCode(fields[0], leftFilesToLoad);
+    const rightCode = await getCode(fields[1], rightFilesToLoad);
 
-    // readFile(resultFile, 'utf8' , (err, data) => {
-    //     if (err) {
-    //         console.error(err);
-    //         return;
-    //     }
-    //
-    //     const lines = data.split("\n").filter(line => line);
-    //
-    //     createIndex(outCourseDir, kmom, lines, submissionsDir);
-    // });
+    htmlContent += "<div class='code-container'>";
+        htmlContent += "<div class='left-code'>";
+        htmlContent += leftCode;
+        htmlContent += "</div>";
+        htmlContent += "<div class='right-code'>";
+        htmlContent += rightCode;
+        htmlContent += "</div>";
+    htmlContent += "</div></div>";
+    htmlContent += footerCompare;
 
-    htmlContent += footer;
+    await fsPromises.writeFile(entireFileName, htmlContent);
+    console.log("Created: " + entireFileName);
+}
+
+async function getCode(studentName, filesToLoad) {
+    let code = `<h2>${studentName}</h2>`;
+    const fileNames = Object.keys(filesToLoad);
+
+    await Promise.all(fileNames.map(async (file) => {
+        const fileEnding = file.split(".")[2];
+        const fileContent = await fsPromises.readFile(file, 'utf8');
+        const linesToHighlight = filesToLoad[file].join(", ");
+
+        code += `<p>${file}</p>`;
+        code += `<pre class="line-numbers language-${fileEnding}" data-line="${linesToHighlight}"><code class="language-${fileEnding}">${fileContent}</code></pre>`;
+    }));
+
+    return code;
 }
 
 parseFiles();
